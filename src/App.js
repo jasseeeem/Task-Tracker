a7343acdad90da6a384c46c73b45b4c0c516b44a
Jaseem001 <jaseemseems@gmail.com>
2021-04-05 19:47:34 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -10,92 +10,121 @@ const App = () => {
   const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([]);
 
-  // useEffect(() => {
-  //   fetch("https://task-tracker-react-flask.herokuapp.com/api/tasks")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setTasks(data.task);
-  //     });
-  //     console.log('fetched')
-  // }, []);
+  useEffect(() => {
+    fetch("https://task-tracker-react-flask.herokuapp.com/api/tasks")
+      .then((res) => res.json())
+      .then((data) => {
+        setTasks(data.task);
+      });
+      console.log('fetched')
+  }, []);
 
-  // //Fetch Task
-  // const fetchTask = async (id) => {
-  //   const res = await fetch(`/tasks/${id}`);
-  //   const data = await res.json();
-  //   return data;
-  // };
+  //Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  };
 
-  // //Delete Task
-  // const deleteTask = async (id) => {
-  //   await fetch(`/tasks/${id}`, {
-  //     method: "DELETE",
-  //   });
-  //   setTasks(tasks.filter((task) => task.id !== id));
-  // };
+  //Delete Task
+  const deleteTask = async (id) => {
+    await fetch(`/tasks/${id}`, {
+      method: "DELETE",
+    });
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
-  // //Toggle Reminder
-  // const toggleReminder = async (id) => {
-  //   const taskToToggle = await fetchTask(id);
-  //   const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
-  //   const res = await fetch(`/tasks/${id}`, {
-  //     method: "PUT",
-  //     headers: {
-  //       "Content-type": "application/json",
-  //     },
-  //     body: JSON.stringify(updTask),
-  //   });
-  //   const data = await res.json();
-  //   setTasks(
-  //     tasks.map((task) =>
-  //       task.id === id ? { ...task, reminder: data.reminder } : task
-  //     )
-  //   );
-  // };
+  //Toggle Reminder
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+    const res = await fetch(`/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updTask),
+    });
+    const data = await res.json();
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      )
+    );
+  };
 
-  // //Edit Task
-  // const onEdit = async (id, task) => {
-  //   const taskToEdit = await fetchTask(id);
-  //   const updTask = { ...taskToEdit, text: task.text, day: task.day };
-  //   const res = await fetch(`/tasks/${id}`, {
-  //     method: "PUT",
-  //     headers: {
-  //       "Content-type": "application/json",
-  //     },
-  //     body: JSON.stringify(updTask),
-  //   });
-  //   const data = await res.json();
-  //   setTasks(
-  //     tasks.map((task) =>
-  //       task.id === id ? { ...task, text: data.text, day: data.day } : task
-  //     )
-  //   );
-  // };
+  //Edit Task
+  const onEdit = async (id, task) => {
+    const taskToEdit = await fetchTask(id);
+    const updTask = { ...taskToEdit, text: task.text, day: task.day };
+    const res = await fetch(`/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updTask),
+    });
+    const data = await res.json();
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, text: data.text, day: data.day } : task
+      )
+    );
+  };
 
-  // //Add Task
-  // const addTask = async (task) => {
-  //   const res = await fetch(
-  //     "https://task-tracker-react-flask.herokuapp.com/api/tasks/add",
-  //     {
-  //       method: "PUT",
-  //       headers: {
-  //         "Content-type": "application/json",
-  //       },
-  //       body: JSON.stringify(task),
-  //     }
-  //   );
-  //   const data = await res.json();
-  //   console.log(data);
+  //Add Task
+  const addTask = async (task) => {
+    const res = await fetch(
+      "https://task-tracker-react-flask.herokuapp.com/api/tasks/add",
+      {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(task),
+      }
+    );
+    const data = await res.json();
+    console.log(data);
 
-  //   setTasks([...tasks, data]);
+    setTasks([...tasks, data]);
 
-  //   setShowAddTask(!showAddTask);
-  // };
+    setShowAddTask(!showAddTask);
+  };
 
   return (
     <Router>
-     <h1>
-Hii</h1>    </Router>
+      <div className="container">
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+        <Route
+          path="/"
+          exact
+          render={(props) => (
+            <>
+              {showAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? (
+                <Tasks
+                  tasks={tasks}
+                  onDelete={deleteTask}
+                  onToggle={toggleReminder}
+                  onEdit={onEdit}
+                />
+              ) : (
+                "No tasks to show"
+              )}
+            </>
+          )}
+        />
+        <Route
+          path="/about"
+          component={About}
+        />
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
